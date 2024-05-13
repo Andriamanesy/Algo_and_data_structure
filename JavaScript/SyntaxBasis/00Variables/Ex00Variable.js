69215d32570ce17b147a0dc73a4ddf8222c872a4
Brian <andriamanesybrian@gmail.com>
2024-05-13 19:29:13 +0300

    Revision javascript(basics) variable

---
+++ JavaScript/SyntaxBasis/00Variables/Ex00Variable.js
@@ -0,0 +1,19 @@
+/*
+Dans l'exercice ci-dessous vous devez créer les variables suivantes :
+
+num qui contient le nombre 255
+txt qui contient la chaîne de caractère 255
+bin qui contient la valeur booléenne vraie
+flt qui contient la valeur 7,23
+*/
+
+
+let num = 255;
+let txt = "255";
+let bin = true;
+let flt = 7.23;
+
+console.log(num);
+console.log(txt + " " + "je suis string");
+console.log(bin);
+console.log(flt);
